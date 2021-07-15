1489f765150d3ea55c00ca7afc6d0e2bb5055d41
vinoth-aeq <vinoth@unilend.finance>
2021-07-15 15:29:52 +0530

    Search filter in selct coin

--- src/component/UI/modal/selectCoinModal.tsx
+++ src/component/UI/modal/selectCoinModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { ThemeContext } from "context/themeContext";
 import EthereumLogo from "assets/ethereumLogo.svg";
 import { Modal, FormControl } from "react-bootstrap";
@@ -7,7 +7,10 @@ import { coinList } from "common";
 
 const SelectCoinModal = (props) => {
   const data: any = useContext(ThemeContext);
-
+  const[searchCoin,setSearchCoin]=useState("");
+const searchCoinHandler=(event)=>{
+  setSearchCoin(event.target.value);
+}
   return (
     <div>
       <Modal
@@ -22,15 +25,23 @@ const SelectCoinModal = (props) => {
               className="custom-coin-search"
               type="text"
               placeholder="Search by name or address"
+              onChange={searchCoinHandler}
             />
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="custom-coin-body">
-          {coinList.map((list) => (
+          {coinList.filter((list)=>{
+            if(searchCoin==""){
+              return list;
+            }else if(list.name.toLowerCase().includes(searchCoin.toLowerCase())){
+              return list;
+            }
+          }).map((list) => ( 
             <div
               className="selectcoin-list"
               onClick={() => {
                 props.handleCoinChange(list.id);
+                setSearchCoin("");
               }}
             >
               <div className="custom-coinlist-div">
